7b6a4e3ac71c6ff302b7319bcaa67b3be127b6c7
UjwalSintre <ujwalnsprime@gmail.com>
2024-01-15 12:04:50 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,83 +1,105 @@
 import { useState } from "react"
-import axios from "axios"
+
+const key = import.meta.env.VITE_KEY
 
 
 
- 
 
 
+function App() {
 
-function App (){
-
-  const [humid,setHumid] = useState(0)
-  const [temp,setTemp] = useState(0)
-  const [wind,setWind] = useState(0)
-  const[city,setCity] = useState()
-
+  const [humid, setHumid] = useState(0)
+  const [temp, setTemp] = useState(0)
+  const [wind, setWind] = useState(0)
+  const [city, setCity] = useState()
+  const [pre, setPre] = useState()
+  const [dis, setDis] = useState(false)
+  const [wi,setWi] = useState()
 
   const getName = (e) => {
+    setDis(false)
     setCity(e.target.value)
-    console.log(city)
+  }
+
+  async function getWeather() {
+
+    setDis(true)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
+      .catch(err => { alert("Error..") })
+    var data = await response.json()
+
+    console.log(data)
+    setPre(data.main.pressure)
+    setCity(data.name)
+    setHumid(data.main.humidity)
+    setTemp(data.main.temp)
+    setWind(data.wind.speed)
+
+
+    if (data.weather[0].main == "Clouds"){
+      setWi("/clouds.png")
     }
-    
-     function getWeather() {
-  
-      const options = {method: 'GET', headers: {accept: 'application/json'}};
+    else if(data.weather[0].main == "Clear"){
+      setWi("/clear.png")
+    }
+    else if(data.weather[0].main == "Rain"){
+      setWi("/rain.png")
+    }
 
-      fetch('https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=LPUlX8S6smN9rq5OBbrQuhh489UHkqih', options)
-        .then(response => {
+    else if(data.weather[0].main == "Drizzle"){
+      setWi("/drizzle.png")
+    }
+    else if(data.weather[0].main == "Mist"){
+      setWi("/mist.png")
+    }
+    else if(data.weather[0].main == "Snow"){
+      setWi("/snow.png")
+    }
 
-          const result = response.json()
-          setCity(result.location.name)
-          console.log(city)
-        })
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    
-    
     }
 
   return (
     <div className=" bg-black w-screen h-screen flex flex-row justify-center ">
-    <div className="w-[1389px] h-[832px]">
-      <div className="relative w-[970px] h-[654px] top-[89px] left-[210px] bg-[#4e4e5066] rounded-[20px]">
-        <div className="absolute w-[425px] h-[72px] top-[126px] left-[274px] font-mono text-8xl text-yellow-400"> NewYork </div>
-        <div className="absolute w-[246px] h-[348px] top-[234px] left-[362px]">
-          <div className="absolute w-[246px] top-0 left-0 font-sans font-normal text-white text-[50px] text-center tracking-[0] leading-[normal]">
-            19:12:98
+      <div className="w-[1389px] h-[832px]">
+        <div className="relative w-[970px] h-[654px] top-[89px] left-[210px] bg-[#4e4e5066] rounded-[20px]">
+          <div className="flex justify-center pt-[128px]  w-auto h-[72px] font-mono text-8xl text-center text-yellow-400">{dis ? city : ""}</div>
+          <div className="absolute  top-[230px] left-[328px] font-sans font-normal  text-white text-4xl tracking-[0] leading-[normal]">
+            {dis ? `Pressure :${pre} hPa` : ""}
           </div>
-          <img className="absolute w-[245px] h-[245px] top-[57px] left-0 " src="" />
-          <div className="absolute w-[198px] top-[310px] left-[24px] font-mono font-normal text-white text-[50px] text-center tracking-[0] leading-[normal]">
-            24°C
+          <div className="absolute  h-[348px] top-[234px] left-[362px]">
+
+            <img className="flex w-[224px] h-[224px] mt-[80px] ml-[5px] left-[30px]" src={dis? wi : "/blank.png"} />
+            <div className="absolute w-[198px] top-[310px] left-[24px] font-mono font-normal text-white text-[50px] text-center tracking-[0] leading-[normal]">
+              {dis ? `${temp}°C` : ""}
+            </div>
           </div>
+          <div className="absolute w-[202px] h-[178px] top-[389px] left-[157px]">
+            <div className="relative w-[198px] h-[178px]">
+              <div className="absolute w-[178px] h-[178px] top-0 left-[10px] bg-[#1a1a1d] rounded-[23px] duration-300  hover:bg-gray-800 border-4 border-black" />
+              <div className="absolute w-[145px] top-[119px] left-[27px] text-3xl font-mono text-white text-center tracking-[0] leading-[normal]">
+                {dis ? "Humidity" : ""}
+              </div>
+              <div className="absolute w-[198px] top-[45px] left-0 text-6xl font-mono text-white text-center tracking-[0] leading-[normal]">
+                {dis ? `${humid}%` : ""}
+              </div>
+            </div>
+          </div>
+          <div className="absolute w-[202px] h-[178px] top-[397px] left-[609px]">
+            <div className="relative w-[198px] h-[178px]">
+              <div className="absolute w-[178px] h-[178px] top-0 left-[10px] bg-[#1a1a1d] rounded-[23px] duration-300  hover:bg-gray-800 border-4 border-black" />
+              <div className="absolute w-[145px] top-[114px] left-[27px] text-3xl font-mono text-white text-center tracking-[0] leading-[normal] ">
+                {dis ? "Wind m/s" : ""}
+              </div>
+              <div className="absolute w-[198px] top-[43px] left-0 font-mono text-6xl  text-white text-center tracking-[0] leading-[normal]">
+                {dis ? wind : ""}
+              </div>
+            </div>
+          </div>
+          <input type="text" placeholder="Enter city" className=" text-slate-400 font-mono text-center text-4xl w-[376px] h-[67px] left-[256px] bg-[#36363c] rounded-[18px] absolute top-[36px] placeholder:font-mono  placeholder:text-slate-500" onChange={getName} />
+          <button className="w-[73px] h-[67px] left-[671px] absolute top-[36px] bg-[#36363c] rounded-2xl text-center text-5xl pl-3 pt-0.5 duration-500 hover:bg-slate-600" onClick={getWeather} >🔍</button>
         </div>
-        <div className="absolute w-[202px] h-[178px] top-[389px] left-[157px]">
-          <div className="relative w-[198px] h-[178px]">
-            <div className="absolute w-[178px] h-[178px] top-0 left-[10px] bg-[#1a1a1d] rounded-[23px] duration-300  hover:bg-gray-800 border-4 border-black" />
-            <div className="absolute w-[145px] top-[119px] left-[27px] text-3xl font-mono text-white text-center tracking-[0] leading-[normal]">
-              Humidity
-            </div>
-            <div className="absolute w-[198px] top-[45px] left-0 text-6xl font-mono text-white text-center tracking-[0] leading-[normal]">
-              65%
-            </div>
-          </div>
-        </div>
-        <div className="absolute w-[202px] h-[178px] top-[397px] left-[609px]">
-          <div className="relative w-[198px] h-[178px]">
-            <div className="absolute w-[178px] h-[178px] top-0 left-[10px] bg-[#1a1a1d] rounded-[23px] duration-300  hover:bg-gray-800 border-4 border-black" />
-            <div className="absolute w-[145px] top-[114px] left-[27px] text-3xl font-mono text-white text-center tracking-[0] leading-[normal] ">
-              Wind m/s
-            </div>
-            <div className="absolute w-[198px] top-[43px] left-0 font-mono text-6xl  text-white text-center tracking-[0] leading-[normal]">
-              40
-            </div>
-          </div>
-        </div>
-        <input type="text" placeholder="Enter city" className=" text-slate-400 font-mono text-center text-4xl w-[376px] h-[67px] left-[256px] bg-[#36363c] rounded-[18px] absolute top-[36px] placeholder:font-mono  placeholder:text-slate-500" onChange={getName}/>
-        <button className="w-[73px] h-[67px] left-[671px] absolute top-[36px] bg-[#36363c] rounded-2xl text-center text-5xl pl-3 pt-0.5 duration-500 hover:bg-slate-600" onClick={getWeather} >🔍</button>
       </div>
     </div>
-  </div>
   )
 }
 
